aada01bc98c0839503b56bc1f49b7794c5b54f06
Grumpy_Pig <pigable101@gmail.com>
2015-01-06 17:03:21 +1100

    Update ScoreManager.cs

--- ScoreBoard/ScoreManager.cs
+++ ScoreBoard/ScoreManager.cs
@@ -12,51 +12,19 @@ public class ScoreManager : MonoBehaviour
 		public GameObject prefabR;
 		public static ScoreManager scores;
 
-		public enum TeamColours
+		public enum TeamColours //This is just what team you are on
 		{
 				red,
 				blue
 		}
-		void Awake ()
+		void Awake () //Just setting up our static variable of this so that we don't need to drag in components
 		{
 				scores = this;
 		}
 
 		void Start ()
 		{
-				Initialize ();
-		}
-
-		[System.Serializable]
-		public class Player
-		{
-				public string name;
-				public int playerId;
-				public GameObject prefab;
-				public TeamColours playerTeam;
-				public static int numberOfPlayers = 0;
-				public Dictionary<string, int> stats = new Dictionary<string, int> ();
-
-				public Player (string Name, TeamColours team)
-				{
-						name = Name;
-						playerId = numberOfPlayers;
-						playerTeam = team;
-						numberOfPlayers++;
-						stats.Clear ();
-						stats.Add ("Kills", 0);
-						stats.Add ("Deaths", 0);
-						stats.Add ("Assists", 0);
-						if (team == TeamColours.blue) {
-								prefab = (GameObject)GameObject.Instantiate (scores.prefabB);
-								prefab.name = name;
-								prefab.transform.SetParent (scores.parentB);
-						} else if (team == TeamColours.red) {
-								prefab = (GameObject)GameObject.Instantiate (scores.prefabR);
-								prefab.name = name;
-								prefab.transform.SetParent (scores.parentR);
-						}
-				}
+				Initialize (); //Initializing things
 		}
 		public void AddPlayer (string name, TeamColours team)
 		{
@@ -76,24 +44,9 @@ public class ScoreManager : MonoBehaviour
 				Initialize ();
 		}
 
-		public int GetStatData (int playerID, string stat)
-		{
-				return players [playerID].stats [stat];
-		}
-
-		public string GetName (int playerID)
-		{
-				return players [playerID].name;
-		}
-
-		public TeamColours GetTeam (int playerID)
-		{
-				return players [playerID].playerTeam;
-		}
-
 		public void Initialize ()
 		{
-				Text[] txt = new Text[4];
+				Text[] txt = new Text[4]; //Foreach player if they key kills || deaths || assists does not exist then create one and set it to 0
 				foreach (Player p in players) {
 						if (!p.stats.ContainsKey ("Kills")) {
 								p.stats.Add ("Kills", 0);
@@ -112,11 +65,59 @@ public class ScoreManager : MonoBehaviour
 				}
 		}
 
-		public void ClearData ()
+		public void ClearData () //This is a scary function since it will reset scores but does display a warning incase it goes off
 		{
 				Debug.LogWarning ("RESETTING ALL DATA");
 				foreach (Player p in players) {
 						p.stats.Clear ();
 				}
 		}
+	//The next three functions are all about getting data without going through the list + dictionary
+		public int GetStatData (int playerID, string stat)
+		{
+				return players [playerID].stats [stat];
+		}
+	
+		public string GetName (int playerID)
+		{
+				return players [playerID].name;
+		}
+	
+		public TeamColours GetTeam (int playerID)
+		{
+				return players [playerID].playerTeam;
+		}
+	//This can be a different script but i decided since it is just for this script to reduce the load of scripts i added this here
+	[System.Serializable]
+	public class Player 
+	{
+		public string name;
+		public int playerId;
+		public GameObject prefab;
+		public TeamColours playerTeam;
+		public static int numberOfPlayers = 0;
+		public Dictionary<string, int> stats = new Dictionary<string, int> (); 
+		//Dictionaries are like lists but run slightly different on a key basis
+		
+		public Player (string Name, TeamColours team)
+		{
+			name = Name;
+			playerId = numberOfPlayers;
+			playerTeam = team;
+			numberOfPlayers++;
+			stats.Clear ();
+			stats.Add ("Kills", 0);
+			stats.Add ("Deaths", 0);
+			stats.Add ("Assists", 0);
+			if (team == TeamColours.blue) { //if blue set up for a blue
+				prefab = (GameObject)GameObject.Instantiate (scores.prefabB);
+				prefab.name = name;
+				prefab.transform.SetParent (scores.parentB);
+			} else if (team == TeamColours.red) { //else set up for a red
+				prefab = (GameObject)GameObject.Instantiate (scores.prefabR);
+				prefab.name = name;
+				prefab.transform.SetParent (scores.parentR);
+			}
+		}
+	}
 }
